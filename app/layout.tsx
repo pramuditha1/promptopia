267d1cc6b2b1f2f7af5b11b05459a5a589375537
Pramuditha Jayasinghe <pramuditha.niroshan@circles.asia>
2024-04-04 18:06:32 +0530

    fix: typescript issues with globals.css import and layout component children

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import "@styles/globals.css"
+import { ReactNode } from "react"
+
+export const metaData = {
+    title: "Promptopia",
+    description: "Discover & share AI prompts"
+}
+
+type RootLayoutProps = {
+    children: ReactNode
+}
+
+const RootLayout = ({children}: RootLayoutProps) => {
+  return (
+    <html lang='en'>
+        <body>
+            <div className='main'>
+                <div className='gradient'></div>
+            </div>
+            <main className='app'>
+                {children}
+            </main>
+        </body>
+    </html>
+  )
+}
+
+export default RootLayout
